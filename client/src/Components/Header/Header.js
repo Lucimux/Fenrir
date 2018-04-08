02d8d05a916c117d3a799e5264d032a1e9d0ab33
Lucimux <vicente.borja@alumno.buap.mx>
2018-04-08 15:05:34 -0500

    Poniendo header en su propio componente

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Header.css'
+const Header = () => (
+    <header>
+        <h1 className='title'>Fenrir</h1>                
+    </header>
+)
+
+export default Header
