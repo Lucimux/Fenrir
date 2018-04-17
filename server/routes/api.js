e56fed9f24c646bf24c36f77e2bb7e9c75436650
Lucimux <vicente.borja@alumno.buap.mx>
2018-04-16 21:01:42 -0500

    anadido endpoint para markdown leyendo archivos

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,9 @@ var express = require('express')
 var router = express.Router()
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
+const fs = require('fs')
+const path = require('path')
+const showdown = require('showdown')
 /* GET home page. */
 
 router.post('/register',(req, res, next) => {
@@ -27,19 +30,28 @@ router.post('/login',(req, res, next) => {
             });
         }        
         const token = jwt.sign(user.toJSON(), 'supersecret')
-        return res.json({user, token})
+        return res.status(200).json({user, token})
 		
 	})(req, res, next)
 })
 
-router.get('/', function(req, res, next) {
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
-});
+router.get('/tema/:tema', (req,res) => {
+	try{
+		const tema = req.params.tema				
+		let filePath = path.resolve('../server/markdownFiles')
+		filePath = path.join(filePath, `${tema}.md`)
+		const file = fs.readFileSync(filePath,'utf-8')				
+		// const markDownConverter = new showdown.Converter()
+		// markDownConverter.setOption('noHeaderId',false)
+		// const htmlContent = markDownConverter.makeHtml(file)
+		res.status(200).json({
+			content: file
+		})
+	}catch(error){
+		res.status(400).json({
+			error: error.errno == -2 ? "Tema no encontrado" : "Ha ocurrido un error"
+		})
+	}
+})
 
 module.exports = router;
