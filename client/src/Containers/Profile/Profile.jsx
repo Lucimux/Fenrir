0ae48e6fa3dbf9d4d831eaeb855df8fe40deef88
Lucimux <vicente.borja@alumno.buap.mx>
2018-04-16 20:59:51 -0500

    Contenedor perfil parcialmente completo markdown funcionando y estilos

--- client/src/Containers/Profile/Profile.jsx
+++ client/src/Containers/Profile/Profile.jsx
@@ -1,12 +1,60 @@
 import React, { Component } from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
+import Markdown from 'react-markdown'
+import axios from 'axios'
 import './Profile.css'
 
 export default class Profile extends Component {
+    state = {
+        index: [
+            {
+                name: "Introduccion",
+                key: "Intro"                
+            },
+            {
+                name: "Metodologia Pentesting",
+                key: "Methodology"           
+            },
+            {
+                name:"Inyeccion SQL",
+                key: "SQL"                
+            },
+            {
+                name:"XSS Scripting",
+                key: "XSS"
+            }
+        ],
+        tema: null,
+        htmlMode: 'raw'
+    }
 
+    componentDidMount(){
+        axios.get('http://localhost:3000/api/tema/Intro')
+            .then(tema => {
+                this.setState({
+                    tema: tema.data.content
+                })            
+            })  
+            .catch(err => {
+                console.log(err)
+            })
+    }
     
+    themeClickHandler(tema){        
+        axios.get(`http://localhost:3000/api/tema/${tema}`)
+        .then(tema => {
+            this.setState({
+                tema: tema.data.content
+            })            
+        })  
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
     render() {
+        
         return (
             <section className='profile-grid-layout'>
                 <Header/>
@@ -14,19 +62,22 @@ export default class Profile extends Component {
                     <div className='index'>
                         <h2 className='index-header'>Temario</h2>
                         <ol>
-                            <li>Hola </li>
-                            <li>
-                                Hola                                 
-                            </li>
-                            <li>Hola </li>
-                            <li>Hola </li>
-                            <li>Hola </li>
-                            <li>Hola </li>
-                            <li>Hola Como estas</li>                            
+                            { this.state.index.map((tema)=>(
+                                <li key={tema.key} onClick={() => this.themeClickHandler(tema.key)}>{tema.name}</li>
+                            ))}    
                         </ol>
                     </div>
                     <div className='topic'>
-
+                        <Markdown 
+                            source={this.state.tema} 
+                            skipHtml={this.state.htmlMode === 'skip'}
+                            escapeHtml={this.state.htmlMode === 'escape'}
+                            className='markdown'
+                        />
+                        <div className='btn-controll'>
+                            <button className='btn-prev'>Anterior</button>
+                            <button className='btn-next'>Siguiente</button>
+                        </div>
                     </div>
                 </section>
                 <Footer/>
